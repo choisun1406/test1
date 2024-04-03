e9ac02ae4f2f83a41db1a0bdca8305a3acf59c8a
choisun1406 <163218275+choisun1406@users.noreply.github.com>
2024-04-03 11:33:48 +0900

    Add files via upload

---
+++ hangman4/Hangman4.py
@@ -0,0 +1,71 @@
+import pygame
+import math
+#1. 게임 초기화
+pygame.init()
+#2. 게임창 옵션 설정
+size = [500, 900]
+screen = pygame.display.set_mode(size)
+title = "HANGMAN"
+pygame.display.set_caption(title)
+#3. 게임 내 필요한 설정
+clock = pygame.time.Clock()
+black = (0,0,0)
+white = (255,255,255)
+def tup_r(tup):
+    temp_list = []
+    for a in tup:
+        temp_list.append(round(a))
+    return tuple(temp_list)
+exit = False
+#4. 메인 이벤트
+while not exit:
+#4-1. FPS 설정
+    clock.tick(60)
+#4-2. 각종 입력 감지
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit = True
+#4-3. 입력, 시간에 따른 변환
+#4-4. 그리기
+    screen.fill(black)
+    A=tup_r((0, size[1]*2/3))
+    B=(size[0], A[1])
+    C=(size[0]/6, A[1])
+    D=(C[0],C[0])
+    E=tup_r((size[0]/2, D[1]))
+    pygame.draw.line(screen, white, A, B, 30)
+    pygame.draw.line(screen, white, C, D, 30)
+    pygame.draw.line(screen, white, D, E, 30)
+    ######################################## 4/3일
+    F=tup_r((E[0],E[1]+size[0]/6))
+    pygame.draw.line(screen, white, E, F, 30)
+    r_head=round(size[0]/12)
+    G=(F[0], F[1]+r_head)
+    pygame.draw.circle(screen, white, G, r_head, 30)
+    H=(G[0], G[1]+r_head)
+    I=(H[0], H[1]+r_head/2)
+    pygame.draw.line(screen, white, H, I, 30)
+    I_arm=r_head*2
+    J=(I[0]-I_arm*math.cos(30*math.pi/180),
+       I[1]+I_arm*math.sin(30*math.pi/180))
+    J=tup_r(J)
+    pygame.draw.line(screen, white, I, J, 30)
+    K=(I[0]+I_arm*math.cos(30*math.pi/180),
+       I[1]+I_arm*math.sin(30*math.pi/180))
+    K=tup_r(K)
+    pygame.draw.line(screen, white, I, K, 30)
+    L=(I[0], I[1]+r_head*5 )
+    pygame.draw.line(screen, white, I, L, 30)
+    I_leg=r_head*3
+    M=(L[0]-I_leg*math.cos(60*math.pi/180),
+       L[1]+I_leg*math.sin(60*math.pi/180))
+    N=(L[0]+I_leg*math.cos(60*math.pi/180),
+       L[1]+I_leg*math.sin(60*math.pi/180))
+    M=tup_r(M)
+    N=tup_r(N)  
+    pygame.draw.line(screen, white, L, M, 30) 
+    pygame.draw.line(screen, white, L, N, 30) 
+#4-5. 업데이트
+    pygame.display.flip()
+#5. 게임종료
+pygame.quit()
